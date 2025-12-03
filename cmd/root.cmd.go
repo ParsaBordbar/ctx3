0987169635bd43b7193028de38f6c7830b85bb76
ParsaBordbar <parsab71@gmail.com>
2025-12-04 02:44:45 +0330

    feat(root.cmd): context tree logo for default command

--- cmd/root.cmd.go
+++ cmd/root.cmd.go
@@ -7,13 +7,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var icon = `
+ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖▗▄▄▄▖▗▖  ▗▖▗▄▄▄▖    ▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖
+▐▌   ▐▌ ▐▌▐▛▚▖▐▌  █  ▐▌    ▝▚▞▘   █        █  ▐▌ ▐▌▐▌   ▐▌   
+▐▌   ▐▌ ▐▌▐▌ ▝▜▌  █  ▐▛▀▀▘  ▐▌    █        █  ▐▛▀▚▖▐▛▀▀▘▐▛▀▀▘
+▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌  █  ▐▙▄▄▖▗▞▘▝▚▖  █        █  ▐▌ ▐▌▐▙▄▄▖▐▙▄▄▖
+`
+
 var rootCmd = &cobra.Command{
 	Use:   "ctx3",
-	Short: "ctx3 is a CLI tool to analyze project structure",
+	Short: "ctx3 is a CLI tool to Help you and your favorite LLM understand projects faster!",
 	Long:  `ctx3 helps you visualize and analyze project structure for better understanding (and LLM context).`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("┌──| ctx3 is a CLI tool to analyze project structure |")
-		fmt.Println("├── Available commands:")
+		fmt.Println(icon)
+		fmt.Println("ctx3 is a CLI tool to analyze project structure.")
+		fmt.Println("┌── Available commands:")
 		fmt.Println("├── context [directory]   Analyze project context for LLMs")
 		fmt.Println("└── print [directory]     Print the file tree of the specified directory")	
 	},
